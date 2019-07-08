5f993c65c32449876f91360a5a34b4a5f44278f7
Brittney <3944682+bawilc01@users.noreply.github.com>
2019-07-08 02:54:15 -0400

    a commit was showing in visual studio

---
+++ yetAnotherMovieDB/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace yetAnotherMovieDB
+{
+    public class Movie
+    {
+        //public int Id { get; set; } not sure how to incorporate yet
+        public string Title { get; set; }
+        public string MovieType { get; set; } //want to be enum
+        public int NumOfCopies { get; set; } //want to be an int
+    }
+
+}
---
+++ yetAnotherMovieDB/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Data.SQLite;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace yetAnotherMovieDB
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("Do you want to view your database or add a new movie?");
+            //if statement to view or add
+
+
+
+            // Call the constructor that has no parameters.
+            Movie movie = new Movie(); //hmm, not sure how to use
+            Console.WriteLine("What is your movie name?");
+            movie.Title = Console.ReadLine();
+            Console.WriteLine("What is your movie type? DVD, Bluray, or Digital?");
+            movie.MovieType = Console.ReadLine();
+            //have to convert int to string since NumOfCopies is an int in the Movie class
+            string movieCopies;
+            Console.WriteLine("How many copies do you have?");
+            movieCopies = Console.ReadLine();
+
+            //convert movieCopies from string to int and return answer depending on number of copies owned
+            // Create a list  
+            List<string> movieList = new List<string>();
+
+            
+
+            int copies = movie.NumOfCopies;
+            if (!Int32.TryParse(movieCopies, out copies))
+            {
+                Console.WriteLine("Invalid data input. Only whole numbers accepted. Please try again.");
+                Main(); //is calling Main the best way?
+                //want to continue the loop but continue doesn't work here
+            }
+            else if (copies == 0)
+            {
+                Console.WriteLine("You have to enter a number greater than 1.");
+                Main(); //is calling Main the best way?
+                //want to continue the loop but continue doesn't work here
+            }
+            else if (copies == 1)
+            {
+                Console.WriteLine(copies + " copy of " + movie.Title + " of type " + movie.MovieType + " has been added to your database.");
+            }
+            else
+            {
+                Console.WriteLine(copies + " copies of " + movie.Title + " of type " + movie.MovieType + " has been added to your database.");
+            }
+
+            // Add items using Add method   
+            movieList.Add(movie.Title + ", " + movie.MovieType + ", " + copies.ToString());
+
+            // Show items in list
+            foreach (string movieItem in movieList)
+            {
+                Console.WriteLine(movieItem);
+            }
+
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+}
